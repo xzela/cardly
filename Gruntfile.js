f0a5dcf008cbea755ff37f91a13003fef04cc29c
Zeph Lafassett <zeph@lafassett.com>
2014-01-28 09:23:53 -0800

    Removed test tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,10 @@ module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jones: {
-      foo: [1, 2, 3],
-      bar: 'hello world',
-      gar: false
-    }
+    pkg: grunt.file.readJSON('package.json')
   });
 
   // Default task(s).
   grunt.registerTask('default', []);
-
-  grunt.registerMultiTask('jones', 'Log stuff', function () {
-    grunt.log.writeln(this.target + ': ' + this.data);
-  });
 
 };
