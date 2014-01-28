ee49051de76bd3812f5e62167e0c3438d72f5145
Zeph Lafassett <zeph@lafassett.com>
2014-01-27 17:42:02 -0800

    cutting node install branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,19 @@ module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jones: {
+      foo: [1, 2, 3],
+      bar: 'hello world',
+      gar: false
+    }
   });
 
   // Default task(s).
   grunt.registerTask('default', []);
+
+  grunt.registerMultiTask('jones', 'Log stuff', function () {
+    grunt.log.writeln(this.target + ': ' + this.data);
+  });
 
 };
