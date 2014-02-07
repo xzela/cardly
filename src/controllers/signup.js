3fadde48b6ea02d6136f952b072fbc6035278fd8
Zeph Lafassett <zeph@lafassett.com>
2014-02-07 08:48:33 -0800

    Adding form submission feature, thanks page, and mongo insertion logic

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,3 +1,5 @@
+var mongoose = require('mongoose'),
+	Signup = mongoose.model('signup');
 
 var signup = {
 	get: function (req, res) {
@@ -6,8 +8,25 @@ var signup = {
 	post: function (req, res) {
 		var body = req.body;
 		if (body.email !== undefined && body.name !== undefined) {
-			var json = {name: body.name, email: body.email};
-			res.send(201, json);
+			req.assert('name', 'Your name is required').notEmpty();
+			req.assert('name', 'Your name must be character long').len(1);
+			req.assert('email', 'Your email address is required').notEmpty();
+			req.assert('email', 'Your email address is invalid').isEmail();
+			var errors = req.validationErrors();
+			var json = {name: body.name, email: body.email, errors: errors};
+			if (errors) {
+				res.render('home', json);
+			} else {
+				var user = new Signup({name: body.name, email: body.email});
+				user.save(function (err) {
+					if (err) {
+						console.log(err);
+						res.send(500);
+					}
+					console.log("save ok");
+					res.render('thanks', json);
+				});
+			}
 		} else {
 			res.send(404);
 		}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ var express = require('express'),
 	exphbs  = require('express3-handlebars'),
 	mongoose = require('mongoose'),
 	hbsConfig = require('./config/handlebars.js'),
-	mongoConfig = require('./config/mongo.js');
+	mongoConfig = require('./config/mongo.js'),
+	expressValidator = require('express-validator');
+
 
 
 var app = express();
+
+app.configure(function () {
+	app.set('port', 3000);
+});
 
 app.use(express.static(__dirname));
 
@@ -15,7 +21,7 @@ console.log(hbsConfig);
 app.engine('.hbs', exphbs(hbsConfig));
 app.set('view engine', '.hbs');
 app.use(express.bodyParser());
-
+app.use(expressValidator());
 
 var connect = function () {
   var options = { server: { socketOptions: { keepAlive: 1 } } };
@@ -27,4 +33,6 @@ connect();
 // load the routes last!
 require('./routes.js')(app);
 
-app.listen(3000);
+app.listen(app.get('port'), function () {
+	console.log('Server started on port: ' + app.get('port'));
+});
